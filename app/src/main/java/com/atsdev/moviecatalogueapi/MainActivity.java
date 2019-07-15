69c9bc329e0ceb365224bfd2d997e3694715a3d4
agustiyann <agustiyaninfo@gmail.com>
2019-07-15 23:30:37 +0700

    create main menu

--- app/src/main/java/com/atsdev/moviecatalogueapi/MainActivity.java
+++ app/src/main/java/com/atsdev/moviecatalogueapi/MainActivity.java
@@ -1,9 +1,14 @@
 package com.atsdev.moviecatalogueapi;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.atsdev.moviecatalogueapi.adapters.ViewPagerAdapter;
 import com.atsdev.moviecatalogueapi.fragments.MovieFragment;
@@ -28,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_settings){
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
