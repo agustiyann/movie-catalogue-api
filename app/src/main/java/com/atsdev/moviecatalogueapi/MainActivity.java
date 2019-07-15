083983ffbef2ea98e787533eae40c2024d12ca02
agustiyann <agustiyaninfo@gmail.com>
2019-07-15 12:30:51 +0700

    rablayout pass

--- app/src/main/java/com/atsdev/moviecatalogueapi/MainActivity.java
+++ app/src/main/java/com/atsdev/moviecatalogueapi/MainActivity.java
@@ -1,7 +1,13 @@
 package com.atsdev.moviecatalogueapi;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.atsdev.moviecatalogueapi.adapters.ViewPagerAdapter;
+import com.atsdev.moviecatalogueapi.fragments.MovieFragment;
+import com.atsdev.moviecatalogueapi.fragments.TvShowFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tablayout_id);
+        ViewPager viewPager = findViewById(R.id.viewpager_id);
+
+        String movie = getResources().getString(R.string.tab_movie);
+        String tv_show = getResources().getString(R.string.tab_tvshow);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new MovieFragment(), movie);
+        adapter.AddFragment(new TvShowFragment(), tv_show);
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/atsdev/moviecatalogueapi/adapters/ViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.atsdev.moviecatalogueapi.adapters;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentListTitle = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentListTitle.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitle.get(position);
+    }
+
+    public void AddFragment(Fragment fragment, String Title) {
+        fragmentList.add(fragment);
+        fragmentListTitle.add(Title);
+    }
+}
