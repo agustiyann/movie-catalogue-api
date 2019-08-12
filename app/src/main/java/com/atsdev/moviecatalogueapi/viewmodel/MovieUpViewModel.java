76f82a2e4965c418ebc371790a7d1fe9b3692271
Agus Tiyansyah Syam <47731450+agustiyann@users.noreply.github.com>
2019-08-12 12:41:00 +0700

    Update MovieUpViewModel.java

--- app/src/main/java/com/atsdev/moviecatalogueapi/viewmodel/MovieUpViewModel.java
+++ app/src/main/java/com/atsdev/moviecatalogueapi/viewmodel/MovieUpViewModel.java
@@ -22,7 +22,7 @@ public class MovieUpViewModel extends ViewModel {
     public void setMovie() {
         AsyncHttpClient client = new AsyncHttpClient();
         final ArrayList<MovieUpData> listMovieItem = new ArrayList<>();
-        String API_KEY = "a6beac03fb8ef024b93e511757777e5c";
+        String API_KEY = "Your themoviedb API key";
         String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=" + API_KEY + "&language=en-US&page=1";
 
         client.get(url, new AsyncHttpResponseHandler() {
